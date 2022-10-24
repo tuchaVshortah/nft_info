0e22adb0aa4fc790224c2b82d9c0e770c2997bd6
tuchaVshortah <nukenbai190104@gmail.com>
2022-10-24 18:14:00 +0600

    add a template and basic functionality

--- web_interface.py
+++ web_interface.py
@@ -1,2 +1,11 @@
-from flask import Flask
-import validators
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/search.html", methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
+def hello_world():
+    return render_template("search.html")
+
+if __name__ == "__main__":
+    app.run()
